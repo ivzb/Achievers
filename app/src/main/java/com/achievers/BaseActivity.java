67fc770ae554fe70281b16b4d41fbd986b2789b2
izahariev <izahariev96@gmail.com>
2017-04-15 22:11:50 +0300

    :sparkles: bug fixed with closing realm when rotating device

--- app/src/main/java/com/achievers/BaseActivity.java
+++ app/src/main/java/com/achievers/BaseActivity.java
@@ -8,16 +8,29 @@ import io.realm.Realm;
 public class BaseActivity extends AppCompatActivity {
 
     protected Realm mRealm;
+    private boolean orientationChange = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.mRealm = Realm.getDefaultInstance();
+        if (!this.orientationChange) this.mRealm = Realm.getDefaultInstance();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        this.orientationChange = true;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        this.orientationChange = false;
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        this.mRealm.close();
+        if (!this.orientationChange) this.mRealm.close();
     }
 }
