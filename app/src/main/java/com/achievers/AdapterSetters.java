f2ae8eb84ced849a0533f1a8c7f2b05b037b144d
izahariev <izahariev96@gmail.com>
2017-04-18 22:15:19 +0300

    :sparkles: img src binding adapters now accept resource uri, path or drawable

--- app/src/main/java/com/achievers/AdapterSetters.java
+++ app/src/main/java/com/achievers/AdapterSetters.java
@@ -1,6 +1,8 @@
 package com.achievers;
 
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
@@ -11,12 +13,36 @@ import com.squareup.picasso.Picasso;
 
 public class AdapterSetters {
     // Generic image loader
-    @BindingAdapter({"bind:imageUrl"})
+    @BindingAdapter("bind:imageUrl")
     public static void loadImage(ImageView view, String imageUrl) {
         Picasso.with(view.getContext())
                 .load(imageUrl)
                 .placeholder(R.drawable.ic_cached_black_24dp)
                 .into(view);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageUri(ImageView view, String imageUri) {
+        if (imageUri == null) {
+            view.setImageURI(null);
+        } else {
+            view.setImageURI(Uri.parse(imageUri));
+        }
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageUri(ImageView view, Uri imageUri) {
+        view.setImageURI(imageUri);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageDrawable(ImageView view, Drawable drawable) {
+        view.setImageDrawable(drawable);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageResource(ImageView imageView, int resource){
+        imageView.setImageResource(resource);
     }
 
     // CategoriesFragment
