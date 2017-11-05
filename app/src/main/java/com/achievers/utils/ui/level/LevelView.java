0a3e20ab2aa6eb2c6a99793c923deb10ca80b419
izahariev <izahariev96@gmail.com>
2017-11-05 10:50:57 +0200

    :sparkles: level view

--- app/src/main/java/com/achievers/utils/ui/level/LevelView.java
+++ app/src/main/java/com/achievers/utils/ui/level/LevelView.java
@@ -13,14 +13,13 @@ import com.achievers.R;
 
 public class LevelView extends View {
 
-    private static final int StrokeWidth = 10;
+    private static final int InfoPadding = 20;
 
     private int mProgress;
 
-    private Paint mDelimiterPaint;
     private Paint mCompletedPaint;
-    private Paint mLeftPaint;
-    private Paint mRingPaint;
+    private Paint mPendingPaint;
+    private Paint mInfoPaint;
     private Paint mTextPaint;
 
     public LevelView(Context context) {
@@ -39,29 +38,23 @@ public class LevelView extends View {
     }
 
     private void init(Context context) {
-        mDelimiterPaint = new Paint();
-        mDelimiterPaint.setAntiAlias(true);
-        mDelimiterPaint.setColor(getResources().getColor(R.color.lvDelimiter));
-        mDelimiterPaint.setStrokeWidth(StrokeWidth);
-        mDelimiterPaint.setStyle(Paint.Style.STROKE);
-
         mCompletedPaint = new Paint();
         mCompletedPaint.setAntiAlias(true);
         mCompletedPaint.setColor(getResources().getColor(R.color.lvCompleted));
         mCompletedPaint.setStyle(Paint.Style.FILL);
 
-        mLeftPaint = new Paint();
-        mLeftPaint.setAntiAlias(true);
-        mLeftPaint.setColor(getResources().getColor(R.color.lvPending));
-        mLeftPaint.setStyle(Paint.Style.FILL);
+        mPendingPaint = new Paint();
+        mPendingPaint.setAntiAlias(true);
+        mPendingPaint.setColor(getResources().getColor(R.color.lvPending));
+        mPendingPaint.setStyle(Paint.Style.FILL);
 
-        mRingPaint = new Paint();
-        mRingPaint.setAntiAlias(true);
-        mRingPaint.setColor(getResources().getColor(R.color.lvDelimiter));
-        mRingPaint.setStyle(Paint.Style.FILL);
+        mInfoPaint = new Paint();
+        mInfoPaint.setAntiAlias(true);
+        mInfoPaint.setColor(getResources().getColor(R.color.lvCompleted));
+        mInfoPaint.setStyle(Paint.Style.FILL);
 
         mTextPaint = new TextPaint();
-        mTextPaint.setColor(getResources().getColor(R.color.lvCompleted));
+        mTextPaint.setColor(getResources().getColor(R.color.lvDelimiter));
         mTextPaint.setAntiAlias(true);
         mTextPaint.setTypeface(ResourcesCompat.getFont(context, R.font.open_sans_bold));
         mTextPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.lv_font_size));
@@ -82,48 +75,56 @@ public class LevelView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        drawCompleted(canvas);
-        drawLeft(canvas);
-        drawInfo(canvas);
+        float width = canvas.getWidth();
+        float height = canvas.getHeight();
+
+        drawCompleted(canvas, width, height);
+        drawPending(canvas, width, height);
+        drawInfo(canvas, width, height);
     }
 
-    private void drawInfo(Canvas canvas) {
-        float percentage = mProgress / 100f;
-
-        String text = String.valueOf(mProgress) + "%";
-
-        float left = (canvas.getWidth() * percentage);
-        float top = canvas.getHeight() * 0.25f;
-        float right = left + mTextPaint.measureText(text);
-        float bottom = canvas.getHeight() * 0.75f;
-
-        canvas.drawRect(left, top, right, bottom, mRingPaint);
-
-        float x = left;
-        float y = (canvas.getHeight() - (mTextPaint.descent() + mTextPaint.ascent())) / 2;
-
-        canvas.drawText(text, x, y, mTextPaint);
-    }
-
-    private void drawCompleted(Canvas canvas) {
+    private void drawCompleted(Canvas canvas, float width, float height) {
         float percentage = mProgress / 100f;
 
         float left = 0;
-        float top = canvas.getHeight() * 0.25f;
-        float right = (canvas.getWidth() * percentage);
-        float bottom = canvas.getHeight() * 0.75f;
+        float top = 0;
+        float right = width * percentage;
+        float bottom = height;
 
         canvas.drawRect(left, top, right, bottom, mCompletedPaint);
     }
 
-    private void drawLeft(Canvas canvas) {
+    private void drawPending(Canvas canvas, float width, float height) {
         float percentage = 1 - (mProgress / 100f);
 
-        float left = canvas.getWidth() - (canvas.getWidth() * percentage);
-        float top = canvas.getHeight() * 0.25f;
-        float right = canvas.getWidth();
-        float bottom = canvas.getHeight() * 0.75f;
+        float left = width - (width * percentage);
+        float top = 0;
+        float right = width;
+        float bottom = height;
 
-        canvas.drawRect(left, top, right, bottom, mLeftPaint);
+        canvas.drawRect(left, top, right, bottom, mPendingPaint);
+    }
+
+    private void drawInfo(Canvas canvas, float width, float height) {
+        float percentage = mProgress / 100f;
+        String text = String.valueOf(mProgress) + "%";
+
+        float textWidth = mTextPaint.measureText(text) + InfoPadding;
+
+        float left = (width * percentage) - textWidth;
+
+        if (left < 0) left = 0;
+        if (left + textWidth > width) left = width - textWidth;
+
+        float top = 0;
+        float right = left + mTextPaint.measureText(text) + InfoPadding;
+        float bottom = height;
+
+        canvas.drawRect(left, top, right, bottom, mInfoPaint);
+
+        float x = left + (InfoPadding / 2);
+        float y = (height - (mTextPaint.descent() + mTextPaint.ascent())) / 2;
+
+        canvas.drawText(text, x, y, mTextPaint);
     }
 }
